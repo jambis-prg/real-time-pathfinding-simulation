5a9f16792b4a9b3471a23ff3610c845b232f4113
shellyda.barbosa <shellyda.barbosa@ifood.com.br>
2026-04-21 12:29:51 -0300

    [feature](BFS): Implement BFS algorithm with initialization and step logic

--- BFS.js
+++ BFS.js
@@ -5,8 +5,32 @@ class BFS extends PathAlgorithm {
   }
 
   init(grid, start, goal) {
+    super.init(grid, start, goal);
+    this.grid.clear();
+    this.queue = [start];
+    this.grid.visit(start, -1);
   }
 
   step() {
+    if (this.queue.length === 0 || this.finished) {
+      this.finished = true;
+      return;
+    }
+
+    let current = this.queue.shift();
+
+    if (current === this.goal) {
+      this.finished = true;
+      return;
+    }
+
+    let neighbors = this.grid.neighbors(current);
+
+    for (let neighbor of neighbors) {
+      if (!this.grid.hasVisited(neighbor)) {
+        this.grid.visit(neighbor, current);
+        this.queue.push(neighbor);
+      }
+    }
   }
 }
